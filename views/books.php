08cfe0b36226429e34e5c87c70a7754bbd3f34af
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-10-31 16:00:43 +0100

    commit3

--- views/books.php
+++ views/books.php
@@ -1,6 +1,20 @@
 <?php $title="Liste des livres";?>
 <?php ob_start();?>
 <h1>Liste des livres</h1>
-<p>Bienvenue sur la page de la liste des livres<p>
-  <?php $content=ob_get_clean();?>
-  <?php require('public/index.php');?>
+
+<ul>
+  <?php
+  foreach ($books as $book) {
+    ?>
+    <li><?php echo $book['title']; ?></li>
+    <?php
+    }
+  ?>
+
+</ul>
+
+<pre>
+  <?php var_dump($books);?>
+</pre>
+<?php $content=ob_get_clean();?>
+<?php require('public/index.php');?>
