482fc4bf4667cecbc65973d048bd0692456e98f6
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-10-31 16:49:59 +0100

    commit4

--- views/books.php
+++ views/books.php
@@ -2,16 +2,35 @@
 <?php ob_start();?>
 <h1>Liste des livres</h1>
 
-<ul>
-  <?php
-  foreach ($books as $book) {
-    ?>
-    <li><?php echo $book['title']; ?></li>
-    <?php
-    }
-  ?>
 
-</ul>
+<?php
+foreach ($books as $book) {
+  ?>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="container">
+          <div class="card" style="width: 18rem;">
+            <img src=<?php echo $book['imageLink']; ?> class="card-img-top" alt="image">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $book['title']; ?></h5>
+              <div class="col-sm">
+                <p class="card-text"><?php echo $book['link']; ?></p>
+              </div>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <?php
+}
+?>
+
+
 
 <pre>
   <?php var_dump($books);?>
