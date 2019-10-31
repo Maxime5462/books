3a54fef849b230ec352fbbfccfc081f336148f53
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-10-31 16:57:36 +0100

    commit5

--- views/books.php
+++ views/books.php
@@ -1,34 +1,31 @@
 <?php $title="Liste des livres";?>
 <?php ob_start();?>
-<h1>Liste des livres</h1>
+<div class="container">
+  <div class="row">
+    <h1>Liste des livres</h1>
 
 
-<?php
-foreach ($books as $book) {
-  ?>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="container">
-          <div class="card" style="width: 18rem;">
-            <img src=<?php echo $book['imageLink']; ?> class="card-img-top" alt="image">
-            <div class="card-body">
-              <h5 class="card-title"><?php echo $book['title']; ?></h5>
-              <div class="col-sm">
-                <p class="card-text"><?php echo $book['link']; ?></p>
-              </div>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+    <?php
+    foreach ($books as $book) {
+      ?>
+
+      <div class="col-2">
+        <div class="card">
+          <img src=<?php echo $book['imageLink']; ?> class="card-img-top" alt="image">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo $book['title']; ?></h5>
+            <p class="card-text"><?php echo $book['link']; ?></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-
       </div>
-    </div>
-  </div>
 
-  <?php
-}
-?>
+
+      <?php
+    }
+    ?>
+  </div>
+</div>
 
 
 
