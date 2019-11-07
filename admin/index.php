b293bed9b6273b48a30ed19fd3245d0a80c0a156
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2019-11-07 13:28:46 +0100

    commit19

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 require('../utils/db.php');
 $db = dbConnect();
 
-$stmt = $db->prepare('SELECT * FROM authors');
+$stmt = $db->prepare('SELECT * FROM authors ORDER BY name');
 $stmt->execute();
 $authors = $stmt->fetchAll();
 ?>
@@ -20,7 +20,6 @@ $authors = $stmt->fetchAll();
   <div class="container">
     <form action="./" method="post">
       <div class="row">
-
         <div class="col-md-6">
           <div class="form-group">
             <label for="title">Titre</label>
@@ -44,10 +43,54 @@ $authors = $stmt->fetchAll();
               <?php foreach ($authors as $author){?>
                 <option><?php echo $author['name'];?></option>
               <?php } ?>
-              </select>
-            </div>
+            </select>
           </div>
-          <div class="col-md-6">
+          <div class="form-group">
+            <label for="pages">Nombre de pages</label>
+            <input
+            name="pages"
+            type="number"
+            class="form-control"
+            id="pages"
+            >
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="wikipedia_link">Lien wikipedia</label>
+            <input
+            name="wikipedia_link"
+            type="number"
+            class="form-control"
+            id="wikipedia_link"
+            >
+          </div>
+          <div class="form-group">
+            <label for="year">Année de parution</label>
+            <input
+            name="year"
+            type="number"
+            class="form-control"
+            id="year"
+            >
+          </div>
+          <div class="form-group">
+            <label for="language">Langue</label>
+            <input
+            name="language"
+            type="text"
+            class="form-control"
+            id="language"
+            >
+          </div>
+          <div class="form-group">
+            <label for="country">Pays</label>
+            <input
+            name="country"
+            type="text"
+            class="form-control"
+            id="country"
+            >
           </div>
         </div>
       </form>
